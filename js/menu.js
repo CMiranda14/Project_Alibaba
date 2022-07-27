b807242a7436592bcedd5062f9c2fbdf57f242e1
Maria <mrmejia@scl.edu.gt>
2022-07-27 13:35:05 -0600

    Se trabaja menu del proyecto Alibaba-Admin

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+let menu = document.getElementById("menu");
+
+let main =  '<nav class="menu">' +
+                    '<a href="" class="text"> inicio       </a>'  +
+                    '<a href="" class="text"> productos    </a>'  +
+                    '<a href="" class="text"> shippingcard </a>'  +   
+                    '<a href="" class="text"> user         </a>'  + 
+                    '<a href="" class="text"> vendedores   </a>'  +  
+                    '<a href="" class="text"> ordenes      </a>'  +  
+            '</nav>'
+
+menu.innerHTML = main;            
+
+
