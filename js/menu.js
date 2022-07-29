f3f6d783dc50ba544cedc1151738d9e2d7c56649
Maria <mrmejia@scl.edu.gt>
2022-07-28 18:25:56 -0600

    Se agrega logo de Alibaba a menu del proyecto

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 let menu = document.getElementById("menu");
 
-let main =  '<nav class="menu">' +
+/*let main =  '<nav class="menu">' +
                     '<a href="" class="text"> inicio       </a>'  +
                     '<a href="" class="text"> productos    </a>'  +
                     '<a href="" class="text"> shippingcard </a>'  +   
@@ -9,6 +9,15 @@ let main =  '<nav class="menu">' +
                     '<a href="" class="text"> ordenes      </a>'  +  
             '</nav>'
 
-menu.innerHTML = main;            
+menu.innerHTML = main;    */        
 
 
+let main = 
+            '<nav class="menu">' +
+                '<img src="https://cdn.iconscout.com/icon/free/png-256/alibaba-3215360-2673778.png" alt="" class="img">' +
+                '<a href="" class="text"> inicio       </a>'  +
+                '<a href="" class="text"> vendedores   </a>'  +  
+                '<a href="" class="text"> ordenes      </a>'  +  
+            '</nav>'
+
+menu.innerHTML = main;       
