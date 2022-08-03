36ccbd1d24eb49478bb55d6f872381aa64a44943
Cristopher <cdtum@scl.edu.gt>
2022-08-02 19:21:52 -0600

    Se finaliza el proyecto

--- js/menu.js
+++ js/menu.js
@@ -4,19 +4,19 @@ function vinculos(){
         let barra_iz = document.querySelector("#barra_izquierda");
     
         let etiqueta = '<a href="productos.html" class="bt_menu">'+'Productos'+'</a>';
-        let etiqueta1 = '<a href="Proveedor_productos_vendidos.html" class="bt_menu">'+'Proveedor de productos vendidos'+'</a>';
+        let etiqueta1 = '<a href="product_sold_vendor.html" class="bt_menu">'+'Proveedor de productos vendidos'+'</a>';
         let etiqueta2 = '<a href="Categoria_productos.html" class="bt_menu">'+'Categoria de productos'+'</a>';
         /*M*/let etiqueta4 = '<a href="opciones.html" class="bt_menu">'+'Opciones de productos'+'</a>';
 
         let etiqueta5 = '<a href="carts_has_products.html" class="bt_menu">'+'Productos añadidos'+'</a>';
         /*M*/let etiqueta6 = '<a href="categorias.html" class="bt_menu">'+'Categorías'+'</a>';
         /*M*/let etiqueta7 = '<a href="ordenes.html" class="bt_menu">'+'Ordenes'+'</a>';
-        /*M*/let etiqueta8 = '<a href="index.html" class="bt_menu">'+'Pedidos de productos'+'</a>';
+        /*M*/let etiqueta8 = '<a href="orders_has_products.html" class="bt_menu">'+'Pedidos de productos'+'</a>';
         let etiqueta9 = '<a href="pedidos.html" class="bt_menu">'+' Pedidos pagados con tarjeta de crédito'+'</a>';
         let etiqueta10 = '<a href="pedidosRealizadosUsuario.html" class="bt_menu">'+'Pedidos realizados por usuarios'+'</a>';
         let etiqueta11 = '<a href="carrosDeCompras.html" class="bt_menu">'+'carrito de compras'+'</a>';
         let etiqueta12 = '<a href="vendedores.html" class="bt_menu">'+'vendedores'+'</a>';
-        let etiqueta13 = '<a href="" class="bt_menu">'+'Opciones'+'</a>';
+        let etiqueta13 = '<a href="option.html" class="bt_menu">'+'Opciones'+'</a>';
     
         barra_iz.innerHTML = etiqueta + etiqueta1 + etiqueta2 +
                              etiqueta4 + etiqueta5+ etiqueta6+ 
@@ -36,13 +36,19 @@ function menu(){
         let main =      '<img src="https://cdn.iconscout.com/icon/free/png-256/alibaba-3215360-2673778.png" alt="" class="logo">' +
 
         '<ul class="nav">' +
+
                 '<li>'  + 
-                        '<a href="index.html" class="text"> Salir   </a>'  +  
+                        '<a href="anna0.html" class="text">Inicio</a>'  +  
+                '</li>'  + 
+
+                '<li>'  + 
+                        '<a href="index.html" class="text"> Salir </a>'  +  
                 '</li>'  + 
 
                 '<li>'  + 
                         '<a href="filtro.html" class="text"> filtro de búsquedad  </a>'  +  
                 '</li>'  + 
+                
         '</ul>'
 
 menu.innerHTML = main;            
--- js/orders_has_products.js
+++ js/orders_has_products.js
@@ -1,34 +1,86 @@
-let order_has_products = [
+const order_has_products = [
 
     /*order_id*/ /*product_id*/ /*option_id*/ /*cantidad*/
-    [1000,	1200,	1201,	2],
-    [1000,	1200,	1202,	1],
-    [1000,	1300,	1301,	3],
-    [1000,	1300,	1302,	2],
-    [1001,	1400,	1401,	1],
-    [1001,	1400,	1402,	1],
-    [1001,	1500,	1501,	2],
-    [1001,	1500,	1502,	3],
-    [1002,	1600,	1601,	2],
-    [1002,	1600,	1602,	1],
-    [1002,	1700,	1701,	1],
-    [1002,	1700,	1702,	3],
-    [1003,	1800,	1801,	1],
-    [1003,	1800,	1802,	2],
-    [1003,	1900,	1901,	1],
-    [1003,	1900,	1902,	2],
-    [1004,	2000,	2001,	2],
-    [1004,	2000,	2002,	3],
-    [1004,	2100,	2101,	1],
-    [1004,	2100,	2102,	3],
-    [1004,	2200,	2201,	2],
-    [1004,	2200,	2202,	3],
-    [1005,	2300,	2301,	1],
-    [1005,	2300,	2302,	1],
-    [1005,	2400,	2401,	3],
-    [1006,	2400,	2402,	2],
-    [1006,	2500,	2501,	3],
-    [1006,	2500,	2502,	1],
-    [1006,	2600,	2601,	2],
-    [1006,	2600,	2602,	1],
+    {id:1000,	product:1200,	option:1201,	cantidad:2},
+    {id:1000,	product:1200,	option:1202,	cantidad:1},
+    {id:1000,	product:1300,	option:1301,	cantidad:3},
+    {id:1000,	product:1300,	option:1302,	cantidad:2},
+    {id:1001,	product:1400,	option:1401,	cantidad:1},
+    {id:1001,	product:1400,	option:1402,	cantidad:1},
+    {id:1001,	product:1500,	option:1501,	cantidad:2},
+    {id:1001,	product:1500,	option:1502,	cantidad:3},
+    {id:1002,	product:1600,	option:1601,	cantidad:2},
+    {id:1002,	product:1600,	option:1602,	cantidad:1},
+    {id:1002,	product:1700,	option:1701,	cantidad:1},
+    {id:1002,	product:1700,	option:1702,	cantidad:3},
+    {id:1003,	product:1800,	option:1801,	cantidad:1},
+    {id:1003,	product:1800,	option:1802,	cantidad:2},
+    {id:1003,	product:1900,	option:1901,	cantidad:1},
+    {id:1003,	product:1900,	option:1902,	cantidad:2},
+    {id:1004,	product:2000,	option:2001,	cantidad:2},
+    {id:1004,	product:2000,	option:2002,	cantidad:3},
+    {id:1004,	product:2100,	option:2101,	cantidad:1},
+    {id:1004,	product:2100,	option:2102,	cantidad:3},
+    {id:1004,	product:2200,	option:2201,	cantidad:2},
+    {id:1004,	product:2200,	option:2202,	cantidad:3},
+    {id:1005,	product:2300,	option:2301,	cantidad:1},
+    {id:1005,	product:2300,	option:2302,	cantidad:1},
+    {id:1005,	product:2400,	option:2401,	cantidad:3},
+    {id:1006,	product:2400,	option:2402,	cantidad:2},
+    {id:1006,	product:2500,	option:2501,	cantidad:3},
+    {id:1006,	product:2500,	option:2502,	cantidad:1},
+    {id:1006,	product:2600,	option:2601,	cantidad:2},
+    {id:1006,	product:2600,	option:2602,	cantidad:1},
 ];
+
+function carts_has_products(){
+
+    let categorias = document.querySelector("#orders_has_products");
+  
+    let tabla3 = '<table class="Tablab" border="0">'+
+                '<tr class="columnas2">'+'<td class="casillas2">'+'ID Orden'+'</td>'+'<td class="casillas2">'+'ID Producto'+ '</td>' +'<td class="casillas2">'+'Opciones'+ '</td>' +'<td class="casillas2">'+'Cantidad'+ '</td>'  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[0].id  +'</td>'+'<td class="casillasA">'+order_has_products[0].product +'<td class="casillasA">'+order_has_products[0].option +'<td class="casillasA">'+order_has_products[0].cantidad +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[1].id  +'</td>'+'<td class="casillasA">'+order_has_products[1].product +'<td class="casillasA">'+order_has_products[1].option +'<td class="casillasA">'+order_has_products[1].cantidad +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[2].id  +'</td>'+'<td class="casillasA">'+order_has_products[2].product +'<td class="casillasA">'+order_has_products[2].option +'<td class="casillasA">'+order_has_products[2].cantidad +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[3].id  +'</td>'+'<td class="casillasA">'+order_has_products[3].product +'<td class="casillasA">'+order_has_products[3].option +'<td class="casillasA">'+order_has_products[3].cantidad +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[4].id  +'</td>'+'<td class="casillasA">'+order_has_products[4].product +'<td class="casillasA">'+order_has_products[4].option +'<td class="casillasA">'+order_has_products[4].cantidad +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[5].id  +'</td>'+'<td class="casillasA">'+order_has_products[5].product +'<td class="casillasA">'+order_has_products[5].option +'<td class="casillasA">'+order_has_products[5].cantidad +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[6].id  +'</td>'+'<td class="casillasA">'+order_has_products[6].product +'<td class="casillasA">'+order_has_products[6].option +'<td class="casillasA">'+order_has_products[6].cantidad +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[7].id  +'</td>'+'<td class="casillasA">'+order_has_products[7].product +'<td class="casillasA">'+order_has_products[7].option +'<td class="casillasA">'+order_has_products[7].cantidad +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[8].id  +'</td>'+'<td class="casillasA">'+order_has_products[8].product +'<td class="casillasA">'+order_has_products[8].option +'<td class="casillasA">'+order_has_products[8].cantidad +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[9].id  +'</td>'+'<td class="casillasA">'+order_has_products[9].product +'<td class="casillasA">'+order_has_products[9].option +'<td class="casillasA">'+order_has_products[9].cantidad +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[10].id +'</td>'+'<td class="casillasA">'+order_has_products[10].product +'<td class="casillasA">'+order_has_products[10].option +'<td class="casillasA">'+order_has_products[10].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[11].id +'</td>'+'<td class="casillasA">'+order_has_products[11].product +'<td class="casillasA">'+order_has_products[11].option +'<td class="casillasA">'+order_has_products[11].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[12].id +'</td>'+'<td class="casillasA">'+order_has_products[12].product +'<td class="casillasA">'+order_has_products[12].option +'<td class="casillasA">'+order_has_products[12].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[13].id +'</td>'+'<td class="casillasA">'+order_has_products[13].product +'<td class="casillasA">'+order_has_products[13].option +'<td class="casillasA">'+order_has_products[13].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[14].id +'</td>'+'<td class="casillasA">'+order_has_products[14].product +'<td class="casillasA">'+order_has_products[14].option +'<td class="casillasA">'+order_has_products[14].cantidad  +'</tr>'+
+                '</table>';
+  
+  
+                let tabla4 = '<table class="Tablab" border="0">'+
+                '<tr class="columnas2">'+'<td class="casillas2">'+'ID Orden'+'</td>'+'<td class="casillas2">'+'ID Producto'+ '</td>' +'<td class="casillas2">'+'Opciones'+ '</td>' +'<td class="casillas2">'+'Cantidad'+ '</td>'  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[15].id +'</td>'+'<td class="casillasA">'+order_has_products[15].product +'<td class="casillasA">'+order_has_products[15].option +'<td class="casillasA">'+order_has_products[15].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[16].id +'</td>'+'<td class="casillasA">'+order_has_products[16].product +'<td class="casillasA">'+order_has_products[16].option +'<td class="casillasA">'+order_has_products[16].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[17].id +'</td>'+'<td class="casillasA">'+order_has_products[17].product +'<td class="casillasA">'+order_has_products[17].option +'<td class="casillasA">'+order_has_products[17].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[18].id +'</td>'+'<td class="casillasA">'+order_has_products[18].product +'<td class="casillasA">'+order_has_products[18].option +'<td class="casillasA">'+order_has_products[18].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[19].id +'</td>'+'<td class="casillasA">'+order_has_products[19].product +'<td class="casillasA">'+order_has_products[19].option +'<td class="casillasA">'+order_has_products[19].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[20].id +'</td>'+'<td class="casillasA">'+order_has_products[20].product +'<td class="casillasA">'+order_has_products[20].option +'<td class="casillasA">'+order_has_products[20].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[21].id +'</td>'+'<td class="casillasA">'+order_has_products[21].product +'<td class="casillasA">'+order_has_products[21].option +'<td class="casillasA">'+order_has_products[21].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[22].id +'</td>'+'<td class="casillasA">'+order_has_products[22].product +'<td class="casillasA">'+order_has_products[22].option +'<td class="casillasA">'+order_has_products[22].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[23].id +'</td>'+'<td class="casillasA">'+order_has_products[23].product +'<td class="casillasA">'+order_has_products[23].option +'<td class="casillasA">'+order_has_products[23].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[24].id +'</td>'+'<td class="casillasA">'+order_has_products[24].product +'<td class="casillasA">'+order_has_products[24].option +'<td class="casillasA">'+order_has_products[24].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[25].id +'</td>'+'<td class="casillasA">'+order_has_products[25].product +'<td class="casillasA">'+order_has_products[25].option +'<td class="casillasA">'+order_has_products[25].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[26].id +'</td>'+'<td class="casillasA">'+order_has_products[26].product +'<td class="casillasA">'+order_has_products[26].option +'<td class="casillasA">'+order_has_products[26].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[27].id +'</td>'+'<td class="casillasA">'+order_has_products[27].product +'<td class="casillasA">'+order_has_products[27].option +'<td class="casillasA">'+order_has_products[27].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[28].id +'</td>'+'<td class="casillasA">'+order_has_products[28].product +'<td class="casillasA">'+order_has_products[28].option +'<td class="casillasA">'+order_has_products[28].cantidad  +'</tr>'+
+                '<tr class="columnasA">'+'<td class="casillasA">'+order_has_products[29].id +'</td>'+'<td class="casillasA">'+order_has_products[29].product +'<td class="casillasA">'+order_has_products[29].option +'<td class="casillasA">'+order_has_products[29].cantidad  +'</tr>'+
+                '</table>';
+  
+  
+    categorias.innerHTML = tabla3 + tabla4;
+  
+  }
+  
+  
+  
+  carts_has_products();
